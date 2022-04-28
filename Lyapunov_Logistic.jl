c9392c0d5e232ff5355cc334fa1a80a74db5746e
Eduardo Somer <89096909+EduSomer26@users.noreply.github.com>
2022-04-28 17:37:50 -0300

    Update Lyapunov_Logistic.jl

--- Lyapunov_Logistic.jl
+++ Lyapunov_Logistic.jl
@@ -1,11 +1,7 @@
 using DynamicalSystems
 
-l = 0.3 #substituir os parâmetros que queremos analisar
-println("λ = coeficiente de Lyapunov")
-L= LinRange(1.0, 1.6, 16)
-j= 1 
-for m in L
-    λ = lyapunov(Systems.henon(a = m, b = l), 10000, d0 = 1e-7, upper_threshold = 1e-4, Ttr = 100)
-    print(j,"λ para a= ",m," e b= ",l, " é: ", λ,"\n")
-    j= j+1
-end
+parametro = 3.6
+
+ds = Systems.logistic(r = parametro)
+
+print(lyapunovspectrum(ds, 10000))
