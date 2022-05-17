3a648a1e3696c66d72189a1319bb3d11c2c56d72
Eduardo Somer <89096909+EduSomer26@users.noreply.github.com>
2022-05-17 20:50:47 -0300

    Update Lyapunov_Logistic.jl

--- Lyapunov_Logistic.jl
+++ Lyapunov_Logistic.jl
@@ -1,7 +1,19 @@
-using DynamicalSystems
+using DynamicalSystems, CairoMakie, Plots
 
-parametro = 3.6
+r = 0.0:0.0005:4.0
+spectrum = rand(length(r),2)
+ds = Systems.logistic()
 
-ds = Systems.logistic(r = parametro)
+for (i , a) in enumerate(r)
+    set_parameter!(ds, 1, a)
+    spectrum[i,:] .= lyapunovspectrum(ds, 1000; Ttr = 500)
+end
 
-print(lyapunovspectrum(ds, 10000))
+fig = Figure()
+ax = Axis(fig[1,1]; xlabel = L"r", ylabel = L"\lambda")
+for j in 1:2
+    lines!(ax, r, spectrum[:, j])
+end
+fig
+save("LyapunovSpectrum.png", fig) 
+
